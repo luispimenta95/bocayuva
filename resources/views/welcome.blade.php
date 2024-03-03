9cd982d36a37226304a12a41b4d78a91892fa88b
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2024-03-03 00:02:21 -0300

    Ajuste endereços

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -852,83 +852,51 @@
             <!-- End Section Title -->
 
             <div class="container" data-aos="fade-up" data-aos-delay="100">
-                <div class="row gy-4">
-                    <div class="col-lg-6">
+                <div class="row gy-12">
+                    <div class="col-lg-12">
                         <div class="row gy-4">
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <div class="info-item" data-aos="fade" data-aos-delay="200">
-                                    <i class="bi bi-geo-alt"></i>
-                                    <h3>Address</h3>
-                                    <p>A108 Adam Street</p>
-                                    <p>New York, NY 535022</p>
+                                    <h3 class="text-center">Bocayuva Tintas Planaltina</h3>
+                                    <p> <i class="bi bi-house-door-fill"></i> Sof Conjunto D, Lotes 26/28, Setor Norte, Setor de Oficinas</p>
+                                    <br><br>
+                                    <p> <i class="bi bi-geo-alt"></i> Planaltina-DF, 73.340-040</p>
+                                    <br><br>
+                                    <p> <i class="bi bi-telephone"></i> (61) 99999-0000</p>
                                 </div>
                             </div>
                             <!-- End Info Item -->
 
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <div class="info-item" data-aos="fade" data-aos-delay="300">
-                                    <i class="bi bi-telephone"></i>
-                                    <h3>Call Us</h3>
-                                    <p>+1 5589 55488 55</p>
-                                    <p>+1 6678 254445 41</p>
+                                    <h3 class="text-center">Bocayuva Tintas Arapoangas</h3>
+                                    <p> <i class="bi bi-house-door-fill"></i>Quadra 07, Conjunto D, Lote 01, Loja 01</p>
+                                    <br><br>
+                                    <p> <i class="bi bi-geo-alt"></i> Arapoangas-DF, 73.368-074</p>
+                                    <br><br>
+                                    <p> <i class="bi bi-telephone"></i> (61) 99999-0000</p>
                                 </div>
                             </div>
                             <!-- End Info Item -->
 
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <div class="info-item" data-aos="fade" data-aos-delay="400">
-                                    <i class="bi bi-envelope"></i>
-                                    <h3>Email Us</h3>
-                                    <p>info@example.com</p>
-                                    <p>contact@example.com</p>
+                                    <h3 class="text-center">Bocayuva Tintas Formosa</h3>
+                                    <p> <i class="bi bi-house-door-fill"></i>Avenida Maestro João Luiz do Espírito Santo, N° 212, Bairro Formosinha</p>
+                                    <br><br>
+                                    <p> <i class="bi bi-geo-alt"></i> Formosa-GO, 73.813-120</p>
+                                    <br><br>
+                                    <p> <i class="bi bi-telephone"></i> (61) 99999-0000</p>
                                 </div>
                             </div>
                             <!-- End Info Item -->
 
-                            <div class="col-md-6">
-                                <div class="info-item" data-aos="fade" data-aos-delay="500">
-                                    <i class="bi bi-clock"></i>
-                                    <h3>Open Hours</h3>
-                                    <p>Monday - Friday</p>
-                                    <p>9:00AM - 05:00PM</p>
-                                </div>
-                            </div>
+
                             <!-- End Info Item -->
                         </div>
                     </div>
 
-                    <div class="col-lg-6">
-                        <form action="forms/contact.php" method="post" class="php-email-form" data-aos="fade-up" data-aos-delay="200">
-                            <div class="row gy-4">
-                                <div class="col-md-6">
-                                    <input type="text" name="name" class="form-control" placeholder="Your Name" required />
-                                </div>
 
-                                <div class="col-md-6">
-                                    <input type="email" class="form-control" name="email" placeholder="Your Email" required />
-                                </div>
-
-                                <div class="col-md-12">
-                                    <input type="text" class="form-control" name="subject" placeholder="Subject" required />
-                                </div>
-
-                                <div class="col-md-12">
-                                    <textarea class="form-control" name="message" rows="6" placeholder="Message" required></textarea>
-                                </div>
-
-                                <div class="col-md-12 text-center">
-                                    <div class="loading">Loading</div>
-                                    <div class="error-message"></div>
-                                    <div class="sent-message">
-                                        Your message has been sent. Thank you!
-                                    </div>
-
-                                    <button type="submit">Send Message</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <!-- End Contact Form -->
                 </div>
             </div>
         </section>
