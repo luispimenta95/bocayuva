5ce7d302d3a31d6debbe78334770bb7c36319f46
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2024-03-13 12:52:54 -0300

    Ajuste quem somos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,6 +29,9 @@
     <!-- Template Main CSS File -->
     <link href="{{ asset('css/main.css')}}" rel="stylesheet" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 
 <body class="index-page" data-bs-spy="scroll" data-bs-target="#navmenu">
@@ -88,113 +91,60 @@
             <!-- End Section Title -->
 
             <div class="container">
-                <div class="row gy-4">
+                <div class="row ">
                     <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-                        <div class="service-item d-flex">
-                            <div class="icon flex-shrink-0">
-                                <i class="bi bi-briefcase"></i>
+                        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                            <!-- Indicators -->
+                            <ol class="carousel-indicators">
+                                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                                <li data-target="#myCarousel" data-slide-to="1"></li>
+                                <li data-target="#myCarousel" data-slide-to="2"></li>
+                            </ol>
+
+                            <!-- Wrapper for slides -->
+                            <div class="carousel-inner">
+                                <div class="item active">
+                                    <img src="{{URL('img/hero-bg.jpg')}}" alt="" style="width:100%;">
+                                </div>
+
+                                <div class="item">
+                                    <img src="{{URL('img/hero-bg.jpg')}}" alt="" style="width:100%;">
+                                </div>
+
+                                <div class="item">
+                                    <img src="{{URL('img/hero-bg.jpg')}}" alt="" style="width:100%;">
+                                </div>
                             </div>
-                            <div>
-                                <h4 class="title">
-                                    <a href="services-details.html" class="stretched-link">Lorem Ipsum</a>
-                                </h4>
-                                <p class="description">
-                                    Voluptatum deleniti atque corrupti quos dolores et quas
-                                    molestias excepturi sint occaecati cupiditate non provident
-                                </p>
-                            </div>
+
+                            <!-- Left and right controls -->
+                            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                                <span class="glyphicon glyphicon-chevron-left"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                                <span class="glyphicon glyphicon-chevron-right"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
                         </div>
                     </div>
                     <!-- End Service Item -->
 
                     <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
-                        <div class="service-item d-flex">
-                            <div class="icon flex-shrink-0">
-                                <i class="bi bi-card-checklist"></i>
-                            </div>
-                            <div>
-                                <h4 class="title">
-                                    <a href="services-details.html" class="stretched-link">Dolor Sitema</a>
-                                </h4>
-                                <p class="description">
-                                    Minim veniam, quis nostrud exercitation ullamco laboris nisi
-                                    ut aliquip ex ea commodo consequat tarad limino ata
-                                </p>
-                            </div>
-                        </div>
+                        <p class="description">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        </p>
+                        <p class="description">
+                            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                        </p>
+                        <p class="description">
+                            The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+                        </p>
                     </div>
                     <!-- End Service Item -->
 
-                    <div class="col-lg-6" data-aos="fade-up" data-aos-delay="300">
-                        <div class="service-item d-flex">
-                            <div class="icon flex-shrink-0">
-                                <i class="bi bi-bar-chart"></i>
-                            </div>
-                            <div>
-                                <h4 class="title">
-                                    <a href="services-details.html" class="stretched-link">Sed ut perspiciatis</a>
-                                </h4>
-                                <p class="description">
-                                    Duis aute irure dolor in reprehenderit in voluptate velit
-                                    esse cillum dolore eu fugiat nulla pariatur
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Service Item -->
 
-                    <div class="col-lg-6" data-aos="fade-up" data-aos-delay="400">
-                        <div class="service-item d-flex">
-                            <div class="icon flex-shrink-0">
-                                <i class="bi bi-binoculars"></i>
-                            </div>
-                            <div>
-                                <h4 class="title">
-                                    <a href="services-details.html" class="stretched-link">Magni Dolores</a>
-                                </h4>
-                                <p class="description">
-                                    Excepteur sint occaecat cupidatat non proident, sunt in
-                                    culpa qui officia deserunt mollit anim id est laborum
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Service Item -->
 
-                    <div class="col-lg-6" data-aos="fade-up" data-aos-delay="500">
-                        <div class="service-item d-flex">
-                            <div class="icon flex-shrink-0">
-                                <i class="bi bi-brightness-high"></i>
-                            </div>
-                            <div>
-                                <h4 class="title">
-                                    <a href="services-details.html" class="stretched-link">Nemo Enim</a>
-                                </h4>
-                                <p class="description">
-                                    At vero eos et accusamus et iusto odio dignissimos ducimus
-                                    qui blanditiis praesentium voluptatum deleniti atque
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Service Item -->
 
-                    <div class="col-lg-6" data-aos="fade-up" data-aos-delay="600">
-                        <div class="service-item d-flex">
-                            <div class="icon flex-shrink-0">
-                                <i class="bi bi-calendar4-week"></i>
-                            </div>
-                            <div>
-                                <h4 class="title">
-                                    <a href="services-details.html" class="stretched-link">Eiusmod Tempor</a>
-                                </h4>
-                                <p class="description">
-                                    Et harum quidem rerum facilis est et expedita distinctio.
-                                    Nam libero tempore, cum soluta nobis est eligendi
-                                </p>
-                            </div>
-                        </div>
-                    </div>
                     <!-- End Service Item -->
                 </div>
             </div>
