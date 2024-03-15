e82d6d2a2ea5c7778fe2f369ab6210cd9a21c112
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2024-03-15 13:19:10 -0300

    Remoção dados numericos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,17 +62,6 @@
         <!-- Hero Section - Dashboard Page -->
         <section id="hero" class="hero">
             <img src="{{URL('img/logos/logo.png')}}" alt="" data-aos="fade-in" />
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-10">
-                        <h2 data-aos="fade-up" data-aos-delay="100">Welcome to Our Website</h2>
-                        <p data-aos="fade-up" data-aos-delay="200">We are team of talented designers making websites with Bootstrap</p>
-                    </div>
-
-                </div>
-
-            </div>
-
 
         </section>
         <!-- End Hero Section -->
@@ -150,46 +139,7 @@
             </div>
         </section>
         <!-- Stats Section - Dashboard Page -->
-        <section id="stats" class="stats">
-            <img src="{{URL('img/logos/nome-vermelho.png')}}" alt="" data-aos="fade-in" />
 
-            <div class="container position-relative" data-aos="fade-up" data-aos-delay="100">
-                <div class="row gy-4">
-                    <div class="col-lg-3 col-md-6">
-                        <div class="stats-item text-center w-100 h-100">
-                            <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1" class="purecounter"></span>
-                            <p>Clients</p>
-                        </div>
-                    </div>
-                    <!-- End Stats Item -->
-
-                    <div class="col-lg-3 col-md-6">
-                        <div class="stats-item text-center w-100 h-100">
-                            <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" class="purecounter"></span>
-                            <p>Projects</p>
-                        </div>
-                    </div>
-                    <!-- End Stats Item -->
-
-                    <div class="col-lg-3 col-md-6">
-                        <div class="stats-item text-center w-100 h-100">
-                            <span data-purecounter-start="0" data-purecounter-end="1453" data-purecounter-duration="1" class="purecounter"></span>
-                            <p>Hours Of Support</p>
-                        </div>
-                    </div>
-                    <!-- End Stats Item -->
-
-                    <div class="col-lg-3 col-md-6">
-                        <div class="stats-item text-center w-100 h-100">
-                            <span data-purecounter-start="0" data-purecounter-end="32" data-purecounter-duration="1" class="purecounter"></span>
-                            <p>Workers</p>
-                        </div>
-                    </div>
-                    <!-- End Stats Item -->
-                </div>
-            </div>
-        </section>
-        <!-- End Stats Section -->
 
         <!-- Serviços Section - Dashboard Page -->
         <section id="services" class="services">
