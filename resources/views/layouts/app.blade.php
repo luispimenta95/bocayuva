92e35dd4e586fe74c13786cf0268015a3292d3fe
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2025-08-02 13:04:18 -0300

    Ajuste tela admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="pt-BR                        <a href="{{ route('admin.slides.index') }}" class="nav-link {{ request()->routeIs('admin.slides.*') ? 'active' : '' }}">Slides</a>>
+<html lang="pt-BR                        <a href="{{ route('admin.slides.index') }}" class="nav-link {{ request()->routeIs('admin.slides.*') ? 'active' : '' }}"></a>
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
